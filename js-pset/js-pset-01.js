f794a8770978d1349734f2cd65c39b072bb142e8
linporu <qazwsx123456guy@gmail.com>
2025-05-13 02:07:14 +0800

    pset(js): Add js-pset-01.js

---
+++ js-pset/js-pset-01.js
@@ -0,0 +1,19 @@
+const hello = () => {
+  return "Hello world!";
+};
+
+const sayHi = (name) => {
+  return `${name}, Hello World!`;
+};
+
+const add = (num1) => {
+  return num1 + num1;
+};
+
+const addTwo = (num1, num2) => {
+  return num1 + num2;
+};
+
+const addNumberAndString = (num1, num2, num3) => {
+  return `第一加第二個參數加總為 ${num1 + num2}，第三個參數為 ${num3}`;
+};
