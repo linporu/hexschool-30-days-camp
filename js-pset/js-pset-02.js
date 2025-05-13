57fa56cc14b08a0ac78ad929fd08dba2e95c5f1e
linporu <qazwsx123456guy@gmail.com>
2025-05-13 10:04:40 +0800

    pset(js): Implement js-pset-02.js with additional functionality and improvements

---
+++ js-pset/js-pset-02.js
@@ -0,0 +1,19 @@
+const square = (num) => {
+  return parseInt(num.toExponential(2));
+};
+
+const calculate = (num1, num2, num3) => {
+  return num1 + num2 - num3;
+};
+
+const rounding = (num) => {
+  return num.toFixed(2);
+};
+
+const parseNumber = (string) => {
+  return Number(string);
+};
+
+const divisible = (num) => {
+  return num % 2;
+};
