42aca6a045e7e1beb83d762037623c0be321a2be
linporu <qazwsx123456guy@gmail.com>
2025-05-26 15:36:02 +0800

    pset(final): Rename files
    - rename index.html as  blog.html
    
    - rename blog.ts for blog rendering,
    
    - rename BlogRenderer to BlogListRenderer
    
    - update vite.config.ts for new entry point

--- main-pset/final-project/vite.config.ts
+++ main-pset/final-project/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     sourcemap: true,
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "index.html"),
+        blog: resolve(__dirname, "blog.html"),
       },
       output: {
         entryFileNames: "js/[name].[hash].js",
